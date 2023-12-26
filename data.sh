c10e6f289aac2e109a2c088fa837750a789fc3d4
Ya-shu1 <103036816+Ya-shu1@users.noreply.github.com>
2023-12-26 11:55:07 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -6,4 +6,4 @@ sudo git clone https://github.com/GOUSERABBANI44/flask-library-app.git
 cd flask-library-app/
 pip3 install -r requirements.txt
 python3 app.py
-nohup python3 -u ./app.py
+screen -m -d python3 app.py
