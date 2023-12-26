eca113d0dec5d000aeade8ba1cf9b5d2f928046f
Ya-shu1 <103036816+Ya-shu1@users.noreply.github.com>
2023-12-26 12:56:28 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,9 @@
 #bin/bash
 sudo apt update
 sudo apt-get full-upgrade -y
-sudo apt-get install -y python3.10-pip
-sudo git clone https://github.com/GOUSERABBANI44/flask-library-app.git
+sudo apt-get install -y python3-pip
+sudo git clone https://github.com/ajay77777777/flask-library-app.git
 cd flask-library-app/
 pip3 install -r requirements.txt
 python3 app.py
-screen -m -d python3 app.py
+nohup python3 -u ./app.py
